ceaebec2832cf970c80410e066631dec243bdcc4
Amadeusz <xaaq333@gmail.com>
2017-09-14 22:45:00 +0200

    add docstrings

--- project_files/network_files/network_layers.py
+++ project_files/network_files/network_layers.py
@@ -38,13 +38,20 @@ class ConvolutionalLayer(AbstractLayer):
     """
 
     def __init__(self, input_feature_number, output_feature_number, filter_size):
+        """
+        Initializes filters in this layer.
+
+        :param input_feature_number: number of features of data before this layer
+        :param output_feature_number: number of features of data after this layer
+        :param filter_size: size of filter
+        """
         self.__filters = self.__random_initialize_filters(input_feature_number, output_feature_number, filter_size)
 
     @staticmethod
     def __random_initialize_filters(input_feature_number, output_feature_number, filter_size):
         """
         Random initializes filers with numbers in range [-0.5, 0.5]. Size of output filters is as follows:
-        output_features x input_features x filter_size x filter_size
+        output_features x input_features x filter_size x filter_size.
 
         :param input_feature_number: number of features of data before this layer
         :param output_feature_number: number of features of data after this layer
@@ -79,6 +86,12 @@ class PoolingLayer(AbstractLayer):
     """
 
     def __init__(self, pooling_window_size):
+        """
+        Initializes pooling window size of this layer. Its size will be as follows:
+        pooling_window_size x pooling_window_size
+
+        :param pooling_window_size: size of pooling window
+        """
         self.__pooling_window_size = pooling_window_size
 
     def forward_propagation(self, input_data):
@@ -95,13 +108,19 @@ class FullyConnectedLayer(AbstractLayer):
     """
 
     def __init__(self, input_neurons_number, output_neurons_number):
+        """
+        Initializes weights of this layer.
+
+        :param input_neurons_number: number of weights of data before this layer (without biases)
+        :param output_neurons_number: number of weights of data after this layer (without biases)
+        """
         self.__weights = self.__random_initialize_weights(input_neurons_number, output_neurons_number)
 
     @staticmethod
     def __random_initialize_weights(input_neurons_number, output_neurons_number):
         """
         Random initializes weights with numbers in range [-0.5, 0.5]. Size of output weights is as follows:
-        output_features x input_features
+        output_features x input_features.
 
         :param input_neurons_number: number of weights of data before this layer (without biases)
         :param output_neurons_number: number of weights of data after this layer (without biases)
