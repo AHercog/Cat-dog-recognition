3ba4ea46edad87cac9f36d273bb5fd6d2b0da7f8
Amadeusz <xaaq333@gmail.com>
2017-09-14 22:42:24 +0200

    make random initialization of filters and weights in conv and fully connected layers

--- project_files/network_files/network_layers.py
+++ project_files/network_files/network_layers.py
@@ -3,6 +3,8 @@ Module containing all types of layers used in neural network.
 """
 from abc import ABC, abstractmethod
 
+import numpy
+
 
 class AbstractLayer(ABC):
     """
@@ -34,6 +36,24 @@ class ConvolutionalLayer(AbstractLayer):
     """
     Layer that is doing convolution on given data.
     """
+
+    def __init__(self, input_feature_number, output_feature_number, filter_size):
+        self.__filters = self.__random_initialize_filters(input_feature_number, output_feature_number, filter_size)
+
+    @staticmethod
+    def __random_initialize_filters(input_feature_number, output_feature_number, filter_size):
+        """
+        Random initializes filers with numbers in range [-0.5, 0.5]. Size of output filters is as follows:
+        output_features x input_features x filter_size x filter_size
+
+        :param input_feature_number: number of features of data before this layer
+        :param output_feature_number: number of features of data after this layer
+        :param filter_size: size of filter
+        :return: random initialized filters
+        """
+        random_filters = numpy.random.rand(output_feature_number, input_feature_number, filter_size, filter_size)
+        return random_filters
+
     def forward_propagation(self, input_data):
         return input_data
 
@@ -45,6 +65,7 @@ class ReluLayer(AbstractLayer):
     """
     Layer that is applying relu function to given data.
     """
+
     def forward_propagation(self, input_data):
         return input_data
 
@@ -56,6 +77,10 @@ class PoolingLayer(AbstractLayer):
     """
     Layer that does pooling on given data.
     """
+
+    def __init__(self, pooling_window_size):
+        self.__pooling_window_size = pooling_window_size
+
     def forward_propagation(self, input_data):
         return input_data
 
@@ -68,6 +93,23 @@ class FullyConnectedLayer(AbstractLayer):
     Fully connected layer where every neuron from one of adjacent layers of data are connected is connected to every
     neuron in second of adjacent layer of data.
     """
+
+    def __init__(self, input_neurons_number, output_neurons_number):
+        self.__weights = self.__random_initialize_weights(input_neurons_number, output_neurons_number)
+
+    @staticmethod
+    def __random_initialize_weights(input_neurons_number, output_neurons_number):
+        """
+        Random initializes weights with numbers in range [-0.5, 0.5]. Size of output weights is as follows:
+        output_features x input_features
+
+        :param input_neurons_number: number of weights of data before this layer (without biases)
+        :param output_neurons_number: number of weights of data after this layer (without biases)
+        :return: random initialized weights
+        """
+        random_filters = numpy.random.rand(output_neurons_number, input_neurons_number)
+        return random_filters
+
     def forward_propagation(self, input_data):
         return input_data
 
@@ -79,6 +121,7 @@ class SigmoidLayer(AbstractLayer):
     """
     Layer that is applying sigmoid function to given data.
     """
+
     def forward_propagation(self, input_data):
         return input_data
 
