444841c62df0ec173f1cbc89b442f5c62ea6de05
Amadeusz <xaaq333@gmail.com>
2017-09-14 23:53:04 +0200

    create method counting fully connected input and refactor creating layers in network

--- project_files/network_files/neural_network.py
+++ project_files/network_files/neural_network.py
@@ -59,11 +59,33 @@ class NeuralNetworkBuilder(AbstractNeuralNetworkBuilder):
         self.__neural_network.set_network_parameters()
 
     def set_layers(self):
+        fully_connected_input_neurons = self.__count_fully_connected_input(50)
         self.__neural_network \
-            .add_layer(ConvolutionalLayer()).add_layer(ReluLayer()).add_layer(PoolingLayer()) \
-            .add_layer(ConvolutionalLayer()).add_layer(ReluLayer()).add_layer(PoolingLayer()) \
-            .add_layer(ConvolutionalLayer()).add_layer(ReluLayer()).add_layer(PoolingLayer()) \
-            .add_layer(FullyConnectedLayer()).add_layer(FullyConnectedLayer()).add_layer(FullyConnectedLayer())
+            .add_layer(ConvolutionalLayer(1, 50, 5)) \
+            .add_layer(ReluLayer()) \
+            .add_layer(PoolingLayer(2)) \
+            .add_layer(ConvolutionalLayer(50, 40, 5)) \
+            .add_layer(ReluLayer()) \
+            .add_layer(PoolingLayer(2)) \
+            .add_layer(ConvolutionalLayer(40, 30, 5)) \
+            .add_layer(ReluLayer()) \
+            .add_layer(PoolingLayer(2)) \
+            .add_layer(FullyConnectedLayer(fully_connected_input_neurons, 25)) \
+            .add_layer(FullyConnectedLayer(25, 25)) \
+            .add_layer(FullyConnectedLayer(25, 1))
+
+    @staticmethod
+    def __count_fully_connected_input(input_map_size_of_network):
+        """
+        Counts how many neurons will be in first fully connected layer of network.
+
+        :param input_map_size_of_network: size of map on the input of whole network
+        :return: number of neurons in first fully connected layer
+        """
+        map_size_after_first_pooling = numpy.math.floor((input_map_size_of_network - 4) / 2)
+        map_size_after_second_pooling = numpy.math.floor((map_size_after_first_pooling - 4) / 2)
+        map_size_after_third_pooling = numpy.math.floor((map_size_after_second_pooling - 4) / 2)
+        return map_size_after_third_pooling
 
     def get_result(self):
         return self.__neural_network
